c39136824ed80e20dfb99b8cffc2546d4866e4d6
pwl <pwl@KHAKI02.ad.mimuw.edu.pl>
2016-11-24 17:41:33 +0100

    pomidor

--- src/org/ilintar/study/question/event/QuestionAnsweredEvent.java
+++ src/org/ilintar/study/question/event/QuestionAnsweredEvent.java
@@ -1,5 +1,35 @@
 package org.ilintar.study.question.event;
 
+import org.ilintar.study.question.Answer;
+import org.ilintar.study.question.Question;
+
+/* pomidor */
 public class QuestionAnsweredEvent {
 
+	protected Question question;
+	protected Answer answer;
+	
+	public QuestionAnsweredEvent(Question question, Answer answer) {
+		this.question = question;
+		this.answer = answer;
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	public Answer getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(Answer answer) {
+		this.answer = answer;
+	}
+	
+	
+	
 }
