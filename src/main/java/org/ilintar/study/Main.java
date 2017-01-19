e228cc253512a3c4cc603d0067c8bd5e8653ee5a
Piotr Wilkin <piotr.wilkin@syndatis.com>
2017-01-19 17:48:47 +0100

    mavenizacja

--- src/main/java/org/ilintar/study/Main.java
+++ src/main/java/org/ilintar/study/Main.java
@@ -6,12 +6,16 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
+import java.net.URL;
+
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("StudyMainScreen.fxml"));
+			URL res = getClass().getResource("StudyMainScreen.fxml");
+			System.out.println(res);
+			BorderPane root = (BorderPane)FXMLLoader.load(res);
 			Scene scene = new Scene(root,600,300);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
